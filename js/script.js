3663b2fae03b6925b282682535f281d2f08d5cae
riccardococciolo <coccioloriccardo64@gmail.com>
2023-10-30 12:16:15 +0100

    add: level

--- js/script.js
+++ js/script.js
@@ -8,20 +8,41 @@ function generateNumbers(maxNumber) {
 
 console.log(generateNumbers(10));
 
-const easy = generateNumbers(100);
-
 document.querySelector(".button").addEventListener("click", function() {
-    gridGenerator(easy);
+    const level = document.getElementById("mySelect").value;
+    let gridSize = "";
+    let cellSize = "";
+
+    switch (level) {
+        case "easy":
+            gridSize = generateNumbers(100)
+            cellSize = 10;
+            break;
+        case "hard":
+            gridSize = generateNumbers(81)
+            cellSize = 9;
+            break;
+        case "insane":
+            gridSize = generateNumbers(49)
+            cellSize = 7;
+            break;
+    
+        default:
+            break;
+    }
+
+    gridGenerator(gridSize, cellSize);
 })
+
 
 const gridElem = document.querySelector(".grid");
 
-function gridGenerator(maxGrid) {
+function gridGenerator(maxGrid, cellSize) {
+    gridElem.innerHTML = "";
     for (let i = 0; i < maxGrid.length; i++) {
         const curNumber = maxGrid[i];
-        const cell = cellGenerator(curNumber);
-        cell.addEventListener("click", handleCellClick)
-
+        const cell = cellGenerator(curNumber, cellSize);
+        cell.addEventListener("click", handleCellClick);
         gridElem.append(cell);
     }
 }
@@ -29,15 +50,15 @@ function gridGenerator(maxGrid) {
 console.log(gridGenerator);
 
 
-function cellGenerator(innerNumber) {
+function cellGenerator(innerNumber, cellSize) {
     const newCell = document.createElement("div");
     newCell.classList.add("cell");
+    newCell.style.width = `calc(100% / ${cellSize})`;
     newCell.innerHTML = innerNumber;
     return newCell
 }
 
-console.log(cellGenerator(easy));
-
 function handleCellClick() {
         this.classList.add("lightblue");
+
 }
